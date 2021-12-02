88a3f6abdeaa12814af41c5d873e0e50f8c20e99
Dustin Seger <dseger@lcs.com>
2021-12-02 16:01:50 -0500

    Complete Day 1 Part 2

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -3,6 +3,10 @@
 '''
 Requirements:
 pip install pyyaml
+
+My solutions:
+Part 1: 1400 increases
+Part 2: 1429 increases
 '''
 
 import yaml
@@ -15,6 +19,8 @@ def load_yaml_data(data_path):
         return yaml.safe_load(f)
 
 def get_increases_and_decreases(height_data):
+    # Oops, default is 1000...
+    sys.setrecursionlimit(len(height_data) + 100)
     return _get_increases_and_decreases(height_data, 0, 0)
 
 def _get_increases_and_decreases(height_data, increases, decreases):
@@ -27,15 +33,30 @@ def _get_increases_and_decreases(height_data, increases, decreases):
     else:
         return _get_increases_and_decreases(height_data[1:], increases, decreases)
 
+def aggregate_sliding_height_sum(height_data, num_to_compile = 3):
+    result = []
+    if (len(height_data) < num_to_compile):
+        return result
+
+    for index in range(0, len(height_data) - num_to_compile + 1):
+        sliding_sum = sum(height_data[i] for i in range(index, index + num_to_compile))
+        result.append(sliding_sum)
+
+    return result
+
 def main():
     height_data = load_yaml_data(HEIGHT_DATA_PATH)
 
-    # Oops, default is 1000
-    # Let it slide this time....
-    sys.setrecursionlimit(len(height_data) + 100)
     increases, decreases = get_increases_and_decreases(height_data)
+    print("Part 1:")
     print("Increases: " + str(increases))
     print("Decreases: " + str(decreases))
+
+    compiled_height_data = aggregate_sliding_height_sum(height_data)
+    compiled_increases, compiled_decreases = get_increases_and_decreases(compiled_height_data)
+    print("\nPart 2:")
+    print("Increases: " + str(compiled_increases))
+    print("Decreases: " + str(compiled_decreases))
 
 if __name__ == '__main__':
     main()
