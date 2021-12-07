67794e5b87b1dfbab3b3439af505822792d4674c
Dustin Seger <dseger@lcs.com>
2021-12-07 16:10:06 -0500

    Day 4 - Complete Part 1

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+
+'''
+Requirements:
+~none~
+
+My solutions:
+Part 1: 51776
+Part 2: ?
+'''
 import pprint
 pp = pprint.PrettyPrinter(indent = 2)
 
@@ -8,9 +17,18 @@ BINGO_DATA_PATH = './Day4Data.txt'
 LINES_BETWEEN_SECTIONS = 1
 BINGO_BOARD_START = 2
 
+# == General-use ==
+
 def read_file_text(data_path):
     with open(data_path, 'r') as f:
         return f.read()
+
+# Pretty wild solution, retrieved from here:
+# https://stackoverflow.com/questions/8421337/rotating-a-two-dimensional-array-in-python
+def rotate_2d_array(arr):
+    return list(zip(*arr[::-1]))
+
+# == Program-specific ==
 
 def parse_calling_order (raw_bingo_text):
     num_text = raw_bingo_text.split('\n')[0].split(',')
@@ -35,11 +53,73 @@ def parse_bingo_boards (raw_bingo_text, line_start = BINGO_BOARD_START):
     bingo_boards = [b for b in bingo_boards if len(b) > 0]
     return bingo_boards
 
+def bingo_board_has_win(bingo_board):
+    return bingo_board_has_vertical_win(bingo_board) or \
+        bingo_board_has_horizontal_win(bingo_board) or \
+        bingo_board_has_diagonal_win(bingo_board)
+
+def bingo_board_has_vertical_win(bingo_board):
+    rotated_board = rotate_2d_array(bingo_board)
+    return bingo_board_has_horizontal_win(rotated_board)
+
+def bingo_board_has_horizontal_win(bingo_board):
+    return any(all(item["called"] for item in row) for row in bingo_board)
+
+def bingo_board_has_diagonal_win(bingo_board):
+    index = 0
+    left_win = True
+    right_win = True
+
+    while index < len(bingo_board) and index < len(bingo_board[index]):
+        if not bingo_board[index][index]["called"]:
+            left_win = False
+        if not bingo_board[index][-(index+1)]["called"]:
+            right_win = False
+
+        index += 1
+
+    return left_win or right_win
+
+def assign_called(num, bingo_board):
+    for row in bingo_board:
+        for val in row:
+            if num == val["number"]:
+                val["called"] = True
+
+    return bingo_board
+
+def get_first_winning_board_info(calling_order, bingo_boards):
+    for num in calling_order:
+        for bingo_board in bingo_boards:
+            assign_called(num, bingo_board)
+            if bingo_board_has_win(bingo_board):
+                return { "board": bingo_board, "last_number_called": num }
+
+def get_unmarked_sum(bingo_board):
+    sum_val = 0
+
+    for row in bingo_board:
+        for val in row:
+            if not val["called"]: sum_val += val["number"]
+
+    return sum_val
+
+# == Main ==
+
 def main ():
     raw_bingo_text = read_file_text(BINGO_DATA_PATH)
     calling_order = parse_calling_order(raw_bingo_text)
     bingo_boards = parse_bingo_boards(raw_bingo_text)
-    pp.pprint(bingo_boards)
+
+    first_winning_board_info = get_first_winning_board_info(calling_order, bingo_boards)
+    first_winning_board = first_winning_board_info["board"]
+    last_number_called = first_winning_board_info["last_number_called"]
+
+    unmarked_sum = get_unmarked_sum(first_winning_board)
+    final_score = unmarked_sum * last_number_called
+
+    print("Part 1:")
+    print("Score: " + str(final_score))
 
 if __name__ == '__main__':
     main()
--- Day4/Day4UnitTest.py
+++ Day4/Day4UnitTest.py
@@ -9,7 +9,7 @@ TEST_DATA = '''
 17  2 23
 '''
 
-EXPECTED = [
+EXPECTED_PARSE = [
     [
         {
             "number": 38,
@@ -54,12 +54,237 @@ EXPECTED = [
     ],
 ]
 
+TEST_BOARD_NO_WIN = [
+    [
+        {
+            "number": 38,
+            "called": False,
+        },
+        {
+            "number": 94,
+            "called": True,
+        },
+        {
+            "number": 65,
+            "called": False,
+        },
+    ],
+    [
+        {
+            "number": 79,
+            "called": False,
+        },
+        {
+            "number": 81,
+            "called": True,
+        },
+        {
+            "number": 80,
+            "called": True,
+        },
+    ],
+    [
+        {
+            "number": 17,
+            "called": True,
+        },
+        {
+            "number": 2,
+            "called": False,
+        },
+        {
+            "number": 23,
+            "called": False,
+        },
+    ],
+]
+
+TEST_BOARD_WITH_WIN_HORIZONTAL = [
+    [
+        {
+            "number": 38,
+            "called": False,
+        },
+        {
+            "number": 94,
+            "called": False,
+        },
+        {
+            "number": 65,
+            "called": False,
+        },
+    ],
+    [
+        {
+            "number": 79,
+            "called": True,
+        },
+        {
+            "number": 81,
+            "called": True,
+        },
+        {
+            "number": 80,
+            "called": True,
+        },
+    ],
+    [
+        {
+            "number": 17,
+            "called": False,
+        },
+        {
+            "number": 2,
+            "called": False,
+        },
+        {
+            "number": 23,
+            "called": False,
+        },
+    ],
+]
+
+TEST_BOARD_WITH_WIN_VERTICAL = [
+    [
+        {
+            "number": 38,
+            "called": True,
+        },
+        {
+            "number": 94,
+            "called": False,
+        },
+        {
+            "number": 65,
+            "called": False,
+        },
+    ],
+    [
+        {
+            "number": 79,
+            "called": True,
+        },
+        {
+            "number": 81,
+            "called": False,
+        },
+        {
+            "number": 80,
+            "called": False,
+        },
+    ],
+    [
+        {
+            "number": 17,
+            "called": True,
+        },
+        {
+            "number": 2,
+            "called": False,
+        },
+        {
+            "number": 23,
+            "called": False,
+        },
+    ],
+]
+
+TEST_BOARD_WITH_WIN_DIAGONAL_RIGHT = [
+    [
+        {
+            "number": 38,
+            "called": True,
+        },
+        {
+            "number": 94,
+            "called": False,
+        },
+        {
+            "number": 65,
+            "called": False,
+        },
+    ],
+    [
+        {
+            "number": 79,
+            "called": False,
+        },
+        {
+            "number": 81,
+            "called": True,
+        },
+        {
+            "number": 80,
+            "called": False,
+        },
+    ],
+    [
+        {
+            "number": 17,
+            "called": False,
+        },
+        {
+            "number": 2,
+            "called": False,
+        },
+        {
+            "number": 23,
+            "called": True,
+        },
+    ],
+]
+
+TEST_BOARD_WITH_WIN_DIAGONAL_LEFT = [
+    [
+        {
+            "number": 38,
+            "called": False,
+        },
+        {
+            "number": 94,
+            "called": False,
+        },
+        {
+            "number": 65,
+            "called": True,
+        },
+    ],
+    [
+        {
+            "number": 79,
+            "called": False,
+        },
+        {
+            "number": 81,
+            "called": True,
+        },
+        {
+            "number": 80,
+            "called": False,
+        },
+    ],
+    [
+        {
+            "number": 17,
+            "called": True,
+        },
+        {
+            "number": 2,
+            "called": False,
+        },
+        {
+            "number": 23,
+            "called": False,
+        },
+    ],
+]
+
 class TestBingoBoards (unittest.TestCase):
     # TODO: Write test for single bingo board?
 
     def test_parse_bingo_boards_should_parse_multiple(self):
         test_data = TEST_DATA + "\n\n" + TEST_DATA
-        expected = [EXPECTED, EXPECTED]
+        expected = [EXPECTED_PARSE, EXPECTED_PARSE]
 
         result = Day4.parse_bingo_boards(test_data, line_start=0)
         self.assertEqual(expected, result)
@@ -70,6 +295,21 @@ class TestBingoBoards (unittest.TestCase):
 
         result = Day4.parse_bingo_boards(test_data, line_start=0)
         self.assertEqual(expected_length, len(result))
+
+    def test_bingo_board_has_win_should_detect_false(self):
+        self.assertFalse(Day4.bingo_board_has_win(TEST_BOARD_NO_WIN))
+
+    def test_bingo_board_has_win_should_detect_true_horizontal(self):
+        self.assertTrue(Day4.bingo_board_has_win(TEST_BOARD_WITH_WIN_HORIZONTAL))
+
+    def test_bingo_board_has_win_should_detect_true_vertical(self):
+        self.assertTrue(Day4.bingo_board_has_win(TEST_BOARD_WITH_WIN_VERTICAL))
+
+    def test_bingo_board_has_win_should_detect_true_diagonal_left(self):
+        self.assertTrue(Day4.bingo_board_has_win(TEST_BOARD_WITH_WIN_DIAGONAL_LEFT))
+
+    def test_bingo_board_has_win_should_detect_true_diagonal_right(self):
+        self.assertTrue(Day4.bingo_board_has_win(TEST_BOARD_WITH_WIN_DIAGONAL_RIGHT))
 
 if __name__ == '__main__':
     unittest.main()
