d72066cd41cb7ce1b53f887d8dd56df2a2e3c872
Dustin Seger <dseger@lcs.com>
2021-12-17 13:33:11 -0500

    Day 7 - Complete Part 2

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -6,7 +6,7 @@ Requirements:
 
 My solutions:
 Part 1: 328187
-Part 2: ?
+Part 2: 91257582
 '''
 import pprint
 import copy
@@ -20,25 +20,39 @@ def read_file_text(data_path):
     with open(data_path, 'r') as f:
         return f.read()
 
+# https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+def triangular_func(n):
+    return (n*(n+1)) / 2
+
 # == Program-specific ==
 
 def import_crab_positions(data_path):
     raw_text = read_file_text(data_path)
     return [int(i) for i in raw_text.split(',')]
 
-def calculate_fuel_use_to(current, destination):
+def calculate_constant_fuel_use(current, destination):
     return abs(current - destination)
 
-def calculate_fuel_uses_to(crab_positions, destination):
-    return sum(calculate_fuel_use_to(c, destination) for c in crab_positions)
+def calculate_triangular_fuel_use(current, destination):
+    dist = abs(current - destination)
+    return triangular_func(dist)
 
-def find_lowest_fuel_use_destination(crab_positions):
+def calculate_fuel_uses(crab_positions, destination, fuel_use_method):
+    return sum(fuel_use_method(c, destination) for c in crab_positions)
+
+def calculate_constant_fuel_uses(crab_positions, destination):
+    return calculate_fuel_uses(crab_positions, destination, calculate_constant_fuel_use)
+
+def calculate_triangular_fuel_uses(crab_positions, destination):
+    return calculate_fuel_uses(crab_positions, destination, calculate_triangular_fuel_use)
+
+def find_lowest_fuel_use_destination(crab_positions, calculate_method):
     min_pos = min(crab_positions)
     max_pos = max(crab_positions)
-    print(min_pos, max_pos)
-    results = {d: calculate_fuel_uses_to(crab_positions, d)
+
+    results = {d: calculate_method(crab_positions, d)
                for d in range(min_pos, max_pos+1)}
-    # pp.pprint(results)
+
     min_dest = min(results, key=results.get)
     fuel_usage = results[min_dest]
     return {
@@ -50,11 +64,17 @@ def find_lowest_fuel_use_destination(crab_positions):
 
 def main ():
     crab_positions = import_crab_positions(CRAB_POSITIONS_PATH)
-    lowest = find_lowest_fuel_use_destination(crab_positions)
+    lowest_constant = find_lowest_fuel_use_destination(crab_positions, calculate_constant_fuel_uses)
 
-    print("Part 1:")
-    print("Best destination: " + str(lowest["dest"]))
-    print("Fuel usage: " + str(lowest["fuel_usage"]))
+    print("Part 1 (Constant fuel use):")
+    print("Best destination: " + str(lowest_constant["dest"]))
+    print("Fuel usage: " + str(lowest_constant["fuel_usage"]))
+
+    lowest_triangular = find_lowest_fuel_use_destination(crab_positions, calculate_triangular_fuel_uses)
+
+    print("Part 2 (Increasing/Triangular fuel use):")
+    print("Best destination: " + str(lowest_triangular["dest"]))
+    print("Fuel usage: " + str(lowest_triangular["fuel_usage"]))
 
 if __name__ == '__main__':
     main()
